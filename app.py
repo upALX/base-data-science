3ff9b302339afbbbce6f0c7a9af94ba8998d911e
José Alexandre <alexandreaddres@gmail.com>
2025-01-21 04:40:17 -0300

    updating deps and changing app main file to deploy

--- app.py
+++ app.py
@@ -36,4 +36,6 @@ def cotacao():
     preco = modelo.predict([dados_input])
     return jsonify(preco=preco[0])
 
-app.run(debug=True, host='0.0.0.0')
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+    
